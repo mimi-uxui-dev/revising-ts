3f14688b244372d634da93b9516a4ac87e794aef
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-10-28 17:22:56 +0100

    callback and fund types

--- app.ts
+++ app.ts
@@ -15,8 +15,19 @@ let combineValues0: Function;
 let combineValues: () => number; // def of this func is that it takes NO valuer and must return a number
 let combineValues1: (a: number, b: number) => number;
 
-combineValues0 = console.log;
+// combineValues0 = console.log;
 
-console.log(combineValues0("miiiled"));
+// console.log(combineValues0("miiiled"));
 
 // combineValues("miiiled");
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  // void mean will ignore any result returning in here
+  const res = n1 + n2;
+  cb(res);
+}
+addAndHandle(1, 7, () => {});
+
+addAndHandle(1, 7, (result) => {
+  console.log(result);
+});
