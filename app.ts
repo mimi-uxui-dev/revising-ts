dd508f4d90119a42d9da7e3289a6c90ed568a2fe
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-10-28 17:37:29 +0100

    the ''never'' type

--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
 let userInput: unknown;
-
-userInput = 5;
-userInput = "max";
+let userName: string;
 
 // any is more flexible than unknown
+userInput = 5;
+userInput = "max";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw {
+    message: message,
+    errorCode: code,
+  }; // it never returns anything bcz ''throw'' crashes the code, it doesnt even return undefiend
+}
