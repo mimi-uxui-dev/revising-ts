52eba77b9719ae7edb68bfdeb4c516bd2a0b278d
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-10-29 16:36:12 +0000

    classes basics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-// Code goes here!
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private emloyees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe() {
+    console.log(`Department (${this.id}): ${this.name} `);
+  }
+
+  addEmployee(employee: string) {
+    this.emloyees.push(employee);
+  }
+  printEmployeeInfo() {
+    console.log(this.emloyees, length);
+    console.log(this.emloyees);
+  }
+}
+
+const accounting = new Department("d1", "Accounting");
+
+accounting.describe();
+accounting.addEmployee("Mimi");
+accounting.addEmployee("Dianna");
+accounting.printEmployeeInfo();
